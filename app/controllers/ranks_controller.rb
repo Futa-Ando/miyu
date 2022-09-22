8ae27b809d202b292597dc668ba2224849e55971
Miyuna <m.miyuna0307@gmail.com>
2022-09-23 02:20:22 +0900

    ○○.index.erbを修正

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,5 +1,7 @@
 class RanksController < ApplicationController
     def index 
      @rank_tweets = Tweet.all.sort {|a,b| b.liked_users.count <=> a.liked_users.count}
+     @rank_tweets = Tweet.limit(10)
+
     end
 end
